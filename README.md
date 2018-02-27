e4005d1aace0ddd0a072da77eedf1d83cfc11a12
Suresh <suresh.durairaj.n@gmail.com>
2018-02-27 17:11:01 +0530

    Readme added

---
+++ README.md
@@ -0,0 +1,4 @@
+This repo will support saving the username/shortname for the given public address in stellarme app (https://github.com/sureshdurairaj/stellarme).
+Clone this repo, then **npm install**.
+TESTNET: Run using **stellarNWType=testnet node . --watch**
+MAINNET: Run using **stellarNWType=mainnet node . --watch**
--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
+const jsonServer = require("json-server");
+const server = jsonServer.create();
 // if (process.env.stellarNWType === 'testnet') {
 //     const router = jsonServer.router('db-test.json')
 // } else {
 // }
-const router = jsonServer.router(process.env.stellarNWType === 'testnet' ? 'db-test.json' : 'db.json')
+const router = jsonServer.router(
+  process.env.stellarNWType === "mainnet" ? "db.json" : "db-test.json"
+);
 
-const middlewares = jsonServer.defaults()
+const middlewares = jsonServer.defaults();
 console.log(process.env.stellarNWType);
 // Set default middlewares (logger, static, cors and no-cache)
-server.use(middlewares)
+server.use(middlewares);
 
 // Add custom routes before JSON Server router
 // server.get('/echo', (req, res) => {
@@ -18,20 +20,19 @@ server.use(middlewares)
 
 // To handle POST, PUT and PATCH you need to use a body-parser
 // You can use the one used by JSON Server
-server.use(jsonServer.bodyParser)
+server.use(jsonServer.bodyParser);
 server.use((req, res, next) => {
-    if (req.method === 'POST' || req.method === 'GET') {
-        req.body.createdAt = Date.now()
-        // Continue to JSON Server router
-        next()
-    } else {
-        res.sendStatus(401)
-    }
-
-})
+  if (req.method === "POST" || req.method === "GET") {
+    req.body.createdAt = Date.now();
+    // Continue to JSON Server router
+    next();
+  } else {
+    res.sendStatus(401);
+  }
+});
 
 // Use default router
 server.use(router);
 server.listen(4000, () => {
-    console.log('JSON Server is running')
-})
+  console.log("JSON Server is running on http://localhost:4000/");
+});
